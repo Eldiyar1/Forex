a4213ba1d8e1a02efce83e9bbdd2e62d3bdd599e
Eldiyar1 <mc.oks1ne@gmail.com>
2024-02-19 18:47:02 +0600

    read_only_fields for user

--- apps/courses/serializers.py
+++ apps/courses/serializers.py
@@ -5,13 +5,14 @@ from .models import Course, Lecture, Review
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ('id', 'user', 'course', 'rating', 'comment', 'created_at')
+        fields = ('id', 'course', 'rating', 'comment', 'created_at')
+        read_only_fields = ('user',)
 
 
 class LectureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lecture
-        fields = ('id', 'title', 'duration', 'video_file')
+        fields = ('id', 'part_number', 'title', 'duration', 'video_file')
 
 
 class BaseCourseSerializer(serializers.ModelSerializer):
@@ -33,11 +34,17 @@ class CourseDetailSerializer(BaseCourseSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     lectures = LectureSerializer(many=True, read_only=True)
     total_duration = serializers.SerializerMethodField()
+    lecture_count = serializers.SerializerMethodField()
 
     class Meta(BaseCourseSerializer.Meta):
         model = Course
-        fields = BaseCourseSerializer.Meta.fields + ('total_duration', 'author', 'lectures', 'reviews')
+        fields = BaseCourseSerializer.Meta.fields + ('total_duration', 'lecture_count', 'lectures', 'reviews')
+        read_only_fields = ('author',)
 
     def get_total_duration(self, obj):
         total_duration = sum(lecture.duration for lecture in obj.lectures.all() if lecture.duration)
         return int(total_duration)
+
+    def get_lecture_count(self, obj):
+        return obj.lectures.count()
+
